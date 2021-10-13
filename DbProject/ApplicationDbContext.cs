77f0e85290f63c14b7338d1623e4a2937f1acfcd
Nicolae Ion Sirbu <nicolaeion.sirbu@endava.com>
2021-10-13 09:07:44 +0300

    remove coments

--- DbProject/ApplicationDbContext.cs
+++ DbProject/ApplicationDbContext.cs
@@ -9,21 +9,8 @@ namespace DbProject
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(connectionString);
-            //ne conectam la o baza de date fie locala fie aflata pe un server strain fie pe cloud cu ajutorul unui connectionString
-            //connection string este un sir de caractere care indica unde se afla baza de date si contine date cum ar fi denumirea
-            //masinii , denumirea bazei de date , in unele cazuri numele si parola contului de admin ale acelei baze de date
-            //pentru a putea efectua operatiuni de scriere si citire
         }
 
-        //lista cu tabele din baza de date
-        public DbSet<SimpleContent> TableOne { get; set; } //creem un tabel cu numele TableOne care o sa contina elemente de tipul SimpleContent
+        public DbSet<SimpleContent> TableOne { get; set; }
     }
-
-    //prin intermediul acestei clase ne conectam la baza de date cu ajutorul unui ORM (Object–relational mapping) EntityFramework care ne permite sa
-    //lucram cu o baza de date de tipul SqlServer direct din cod C#
-
-
-    //daca dorim sa aducem modificari la structura bazei de date , sa adaugam sau stergem tabele sau sa le modificam , dupa ce facem schimbarile necesare
-    //utilizam comanda add-migration "numele migratiei"
-    //dupa update-database
 }
--- DbProject/Program.cs
+++ DbProject/Program.cs
@@ -10,40 +10,38 @@ namespace DbProject
         {
             string command = "";
 
-            //introducem un while pentru a crea ceva de genul la un meniu care va functiona pana tastam "exit"
             while (command != "exit")
             {
-                //curatam consola
                 Console.Clear();
-                Console.WriteLine("-- Baza de date --\n");
+                Console.WriteLine("-- Simple Database --\n");
 
-                using var database = new ApplicationDbContext();   //creem o conectiun la baza de date
-                int elementsCount = 0;  //intitializam un contor sa vedem cate elemente sunt in baza de date
+                using var database = new ApplicationDbContext();
+                int elementsCount = 0;
 
-                if (database.TableOne.Any()) // verificam daca sunt elemente si daca sunt atribuim contorului nr de elemente
+                if (database.TableOne.Any())
                     elementsCount = database.TableOne.Count();
                 else
                     elementsCount = 0;
 
-                Console.WriteLine($"Baza de date contine {elementsCount} elemente"); //afisam numarul de elemente
+                Console.WriteLine($"There are {elementsCount} elements TableOne");
 
-                if (elementsCount > 0) //daca exista elemente afisam elementele din baza de date din tabelul TableOne
+                if (elementsCount > 0)
                     foreach (var obj in database.TableOne)
                     {
-                        Console.WriteLine("Id : " + obj.Id + " | Continut : " + obj.Content);
+                        Console.WriteLine("Id : " + obj.Id + " | Content : " + obj.Content);
                     }
 
-                Console.WriteLine("\nTastati 1 pentru a introduce un elemnt nou sau \"exit\" pentru a iesi");
+                Console.WriteLine("\nPress 1 to add one more element or \"exit\" to end the program");
                 command = Console.ReadLine();
 
-                if (command == "1") //daca dorim sa introducem un element tastam 1
+                if (command == "1")
                 {
                     Console.Clear();
-                    Console.WriteLine("Introduceti continutul noului elemnt : ");
-                    SimpleContent newElement = new(); //creem un element nou de tipul SimpleContent , acest tip de element se accepta in tabelul TableOne din baza de date
-                    newElement.Content = Console.ReadLine(); //initializam campul Content cu continut citit de la tastatura
-                    database.TableOne.Add(newElement); //introducem elementul nou in tabelul TableOne
-                    database.SaveChanges(); //salvam modificarile aduse la baza de date
+                    Console.WriteLine("Content of the new element : ");
+                    SimpleContent newElement = new();
+                    newElement.Content = Console.ReadLine();
+                    database.TableOne.Add(newElement);
+                    database.SaveChanges();
                 }
             }
 
